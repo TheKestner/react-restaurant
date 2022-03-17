9a9472bb4a16a998573957b873238acc93476fdd
Zachary Kestner <zachary.kestner@student.awesomeinc.org>
2022-03-17 15:16:03 +0000

    added nav bar without links

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
 import "./App.css";
 
 
 export default function App() {
   return (
-    <Container className="p-3">
+    // <Container className="p-3">
     <div>
       <h1>KESTNER'S</h1>
-    </div>
-    </Container>
+      <Nav justify variant="tabs" defaultActiveKey="/home">
+  <Nav.Item>
+    <Nav.Link eventKey="disabled" disabled>Menu:</Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+    <Nav.Link eventKey="link-1">Appitizers</Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+    <Nav.Link eventKey="link-2">Lunch</Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+    <Nav.Link eventKey="link-3">
+      Dinner
+    </Nav.Link>
+  </Nav.Item>
+</Nav>
+    </div> 
+   // </Container>
   );
 }
-
 
