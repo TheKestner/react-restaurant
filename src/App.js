c55898a0d778d5da733af2c8efbce747e5dc50b8
Zachary Kestner <zachary.kestner@student.awesomeinc.org>
2022-03-17 01:02:04 +0000

    set up App.js to display content

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import { Link, Outlet } from 'react-router-dom';
+// import { Link, Outlet } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
 import "./App.css";
 
 
 export default function App() {
   return (
+    <Container className="p-3">
     <div>
       <h1>THIS IS A TEST</h1>
     </div>
+    </Container>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter,
+  Route,
+  Routes
+} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 ReactDOM.render(
-  <browserRouter>
+  
+  <BrowserRouter>
    <Routes>
      <Route path="/" element={<App />}>
 
-     </Route>
-   </Routes>
-  </browserRouter>
-  , document.getElementById('app')
+       </Route>
+     </Routes>
+    </BrowserRouter>,
+    document.getElementById('app')
 );
 
 
